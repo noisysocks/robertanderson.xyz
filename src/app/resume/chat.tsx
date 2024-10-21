f379dbd522bf33e30a540bcd8e4085fc6e671928
Robert Anderson <robert@noisysocks.com>
2024-10-22 09:48:51 +1100

    Add error handling to chat

--- src/app/resume/chat.tsx
+++ src/app/resume/chat.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
 import {
   ChatBubble,
@@ -9,22 +10,30 @@ import { ChatInput } from "@/components/ui/chat/chat-input";
 import { ChatMessageList } from "@/components/ui/chat/chat-message-list";
 import { cn } from "@/lib/utils";
 import { useChat } from "ai/react";
-import { Send } from "lucide-react";
+import { AlertCircle, Send } from "lucide-react";
 
 export function Chat({ initialMessage }: { initialMessage?: string }) {
-  const { messages, isLoading, input, handleInputChange, handleSubmit } =
-    useChat({
-      id: "resume",
-      initialMessages: [
-        {
-          id: "initial",
-          content:
-            initialMessage ??
-            "Hi! I'm Robert's digital friend.\n\nTake a look at his resume on the right and ask me any questions you have.",
-          role: "assistant",
-        },
-      ],
-    });
+  const {
+    messages,
+    isLoading,
+    input,
+    handleInputChange,
+    handleSubmit,
+    error,
+    reload,
+  } = useChat({
+    id: "resume",
+    initialMessages: [
+      {
+        id: "initial",
+        content:
+          initialMessage ??
+          "Hi! I'm Robert's digital friend.\n\nTake a look at his resume on the right and ask me any questions you have.",
+        role: "assistant",
+      },
+    ],
+    keepLastMessageOnError: true,
+  });
 
   return (
     <div className="flex h-full flex-col p-4">
@@ -50,6 +59,23 @@ export function Chat({ initialMessage }: { initialMessage?: string }) {
           </ChatBubble>
         )}
       </ChatMessageList>
+      {error && (
+        <Alert variant="destructive" className="my-4">
+          <AlertCircle className="h-4 w-4" />
+          <AlertTitle>Uh oh</AlertTitle>
+          <AlertDescription className="flex flex-col">
+            An unexpected error occurred while sending your message.
+            <Button
+              variant="outline"
+              size="sm"
+              className="mt-2 w-min"
+              onClick={() => reload()}
+            >
+              Try again
+            </Button>
+          </AlertDescription>
+        </Alert>
+      )}
       <form className="relative" onSubmit={handleSubmit}>
         <ChatInput
           className="w-full pr-12"
