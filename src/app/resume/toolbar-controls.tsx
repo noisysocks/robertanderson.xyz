a5fe4ca9248049c4d66b78282a8f0ec2ff44bf66
Robert Anderson <robert@noisysocks.com>
2024-10-22 15:38:58 +1100

    Switch between 'Open chat' and 'Close chat' tooltip

--- src/app/resume/toolbar-controls.tsx
+++ src/app/resume/toolbar-controls.tsx
@@ -7,21 +7,21 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
-import { MessageCircle, Printer } from "lucide-react";
+import { ChevronLeft, MessageCircle, Printer } from "lucide-react";
 import { useInterface } from "./interface";
 
 export function ToolbarControls() {
-  const { toggleSidebar } = useInterface();
+  const { isSidebarOpen, toggleSidebar } = useInterface();
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger asChild>
           <Button variant="outline" size="icon" onClick={toggleSidebar}>
-            <MessageCircle />
+            {isSidebarOpen ? <ChevronLeft /> : <MessageCircle />}
           </Button>
         </TooltipTrigger>
         <TooltipContent>
-          <p>Open chat</p>
+          <p>{isSidebarOpen ? "Close chat" : "Open chat"}</p>
         </TooltipContent>
       </Tooltip>
       <Tooltip>
